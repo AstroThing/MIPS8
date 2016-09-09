77e4a3f3d2e28bb80c9fd90f01cbd0ad44c15bfd
AstroThing <andres.rodriguezhn@gmail.com>
2016-09-09 16:08:07 -0600

    Added Flags module

---
+++ FPGA/Flags.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+module Flags(
+    input flags_write,
+	 input zero,
+	 input sign,
+	 input ovf,
+	 output zf,
+	 output sf,
+	 output of
+    );
+	 
+	 reg [2:0] flags;
+	 assign zf = flags[0];
+	 assign sf = flags[1];
+	 assign of = flags[2];
+	 
+	 always @ (flags_write)
+		if(flags_write)
+		begin
+			flags[0] = zero;
+			flags[1] = sign;
+			flags[2] = ovf;
+		end
+
+endmodule
